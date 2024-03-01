d9acdc727101334518c0d30419bb2f622f208ea6
Kevin <kevinantony03@gmail.com>
2024-03-01 12:39:39 +0530

    Create HTFunnelv1.sql
    Initial funnel for making looker db. Cointains total unique users, phone no given, carousels sent and confirmation count

---
+++ HTFunnelv1.sql
@@ -0,0 +1,59 @@
+WITH UniqueUsers AS (
+  SELECT
+    DATE AS Date,
+    COUNT(DISTINCT user_id) AS Total_Leads
+  FROM  
+    `test-project-122123.holiday_tribe_db.f72973_conv_data`
+  GROUP BY
+    Date
+),
+ExtractedNumbers AS (
+  SELECT
+    DATE(date) AS Date,
+    COUNT(DISTINCT REGEXP_EXTRACT(payload, r'\b\d{10}\b')) AS Phone_Numbers_Captured
+  FROM
+    `test-project-122123.holiday_tribe_db.f72973_conv_data`,
+    UNNEST(REGEXP_EXTRACT_ALL(payload, r'\b\d{10}\b')) AS number
+  GROUP BY
+    Date
+),
+PopularPackagesMessages AS (
+  SELECT
+    DATE(date) AS Date,
+    COUNT(DISTINCT user_id) AS Carousel_Sent
+  FROM
+    `test-project-122123.holiday_tribe_db.f72973_conv_data`
+  WHERE
+    payload LIKE '%Please wait while we find the popular packages%'
+  GROUP BY
+    Date
+),
+InfoGiven AS (
+  SELECT
+    DATE(date) AS Date,
+    COUNT(*) AS Info_Given
+  FROM
+    `test-project-122123.holiday_tribe_db.f72973_conv_data`
+  WHERE
+    payload LIKE '%affirmation%'
+  GROUP BY
+    Date
+)
+
+
+SELECT
+  UU.Date,
+  UU.Total_Leads,
+  COALESCE(ENN.Phone_Numbers_Captured, 0) AS Phone_Numbers_Captured,
+  COALESCE(PPM.Carousel_Sent, 0) AS Carousel_Sent,
+  COALESCE(IG.Info_Given, 0) AS Info_Given
+FROM
+  UniqueUsers UU
+LEFT JOIN
+  ExtractedNumbers ENN ON UU.Date = ENN.Date
+LEFT JOIN
+  PopularPackagesMessages PPM ON UU.Date = PPM.Date
+LEFT JOIN
+  InfoGiven IG ON UU.Date = IG.Date
+ORDER BY
+  UU.Date;
